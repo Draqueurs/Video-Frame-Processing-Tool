b47dc94cd1d9018d9452693dbda446a522a94b40
Draqueurs <61468711+Draqueurs@users.noreply.github.com>
2023-08-01 10:34:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
+"""
+Process Videos Script
+
+This script is designed to process videos from a folder, calculate the Mean Squared Error between frames, and save the frames if the error exceeds a threshold. The script supports various sorting modes for the saved frames.
+
+Functions:
+    main():
+        Main function to process videos based on command-line arguments.
+
+Usage:
+    python main.py -o original_folder_path -d destination_folder_path -t threshold -s sort_mode -r number_of_frame_per_folder -n number_of_folder -c
+
+Arguments:
+    -o, --original-folder-path:
+        Path of the folder containing the videos.
+
+    -d, --destination-folder-path:
+        Output folder path to save frames.
+
+    -t, --threshold:
+        Threshold for Mean Squared Error (default: -1).
+
+    -D, --duration:
+        Real duration of the videos in seconds (default: -1).
+
+    -s, --sort-mode:
+        Sorting mode(s) for the images.
+
+    -r, --number-of-frame-per-folder:
+        Number of frames per folder (default: 4).
+
+    -n, --number-of-folder:
+        Number of subfolders (default: 0).
+
+    -c, --clear:
+        Clear the original folder after processing (default: False).
+"""
+
 import argparse
 import os
 import shutil
@@ -10,25 +48,15 @@ from utils import (
 def main():
     """
     Main function to process videos based on command-line arguments.
-
-    Usage:
-    python script_name.py -o original_folder_path -d destination_folder_path -t threshold -s sort_mode -r number_of_frame_per_folder -n number_of_folder -c
-
-    -o, --original-folder-path: Path of the folder containing the videos.
-    -d, --destination-folder-path: Output folder path to save frames.
-    -t, --threshold: Threshold for Mean Squared Error (default: -1).
-    -s, --sort-mode: Sorting mode(s) for the images.
-    -r, --number-of-frame-per-folder: Number of frames per folder (default: 4).
-    -n, --number-of-folder: Number of folders (default: 0).
-    -c, --clear: Clear the original folder after processing (default: False).
     """
     parser = argparse.ArgumentParser(description="Open videos from a folder, calculate the Mean Squared Error between frames, and save the frame if the error exceeds a threshold.")
     parser.add_argument("-o", "--original-folder-path", help="Path of the folder containing the videos.")
     parser.add_argument("-d", "--destination-folder-path", default=None, help="Output folder path to save frames.")
     parser.add_argument("-t", "--threshold", type=float, default=-1., help="Threshold for Mean Squared Error.")
+    parser.add_argument("-D", "--duration", type=float, default=-1., help="Real duration of the videos in seconds.")
     parser.add_argument("-s", "--sort-mode", nargs='+', help="Sorting mode for the images.")
     parser.add_argument("-r", "--number-of-frame-per-folder", type=int, default=4, help="Number of frame per folder.")
-    parser.add_argument("-n", "--number-of-folder", type=int, default=0, help="Number of folder.")
+    parser.add_argument("-n", "--number-of-folder", type=int, default=0, help="Number of subfolders.")
     parser.add_argument("-c", "--clear", default=False, action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
 
@@ -51,8 +79,9 @@ def main():
         destination_folder_path_frames = os.path.join(os.path.dirname(original_folder_path), 'frames')
     print(destination_folder_path_frames)
     threshold = args.threshold
+    duration = args.duration
 
-    process_videos_in_folder(original_folder_path, destination_folder_path_frames, threshold)
+    process_videos_in_folder(original_folder_path, destination_folder_path_frames, threshold, duration)
 
     original_folder_path = destination_folder_path_frames
     if args.destination_folder_path is not None:
